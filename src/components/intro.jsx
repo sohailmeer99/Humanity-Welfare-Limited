ffe0a3ca0aa989c84486f60d1a941f1c4be747e5
sohailmeer99 <sohail.752@uswagb.edu.pk>
2025-03-24 02:40:41 +0500

    deployment error

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -8,8 +8,8 @@ function Intro(){
                 </div>
                 <div>
                     <div><h1>About us</h1></div>
-                    <div><h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non sequi, ipsam alias voluptatem quasi libero.</h3></div>
-                    <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, nulla quisquam sequi, quasi, labore commodi porro ullam iure dolorem a omnis nostrum ipsa vitae assumenda maxime aspernatur atque quas nisi? Facere eligendi reprehenderit vel sint iusto unde maxime enim nostrum ullam. Eveniet mollitia commodi beatae quaerat suscipit, culpa facilis ab.</p></div>
+                    <div><h3>Lorem ipsum dolor sit consectetur, adipisicing elit. Non sequi, ipsam alias voluptatem quasi libero.</h3></div>
+                    <div><p>Lorem ipsum dolor sit consectetur adipisicing elit. Molestias, nulla quisquam sequi, quasi, labore commodi porro ullam iure dolorem a omnis nostrum ipsa vitae assumenda maxime aspernatur atque quas nisi? Facere eligendi reprehenderit vel sint iusto unde maxime enim nostrum ullam. Eveniet mollitia commodi beatae quaerat suscipit, culpa facilis ab.</p></div>
                     <div></div>
                 </div>
             </div>
