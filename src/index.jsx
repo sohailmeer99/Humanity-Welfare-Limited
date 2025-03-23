68ba4b31b5bbd5fe85f0c1e75e5b126b3a2c8292
sohailmeer99 <sohail.752@uswagb.edu.pk>
2025-03-24 01:30:33 +0500

    This commit is about deployment errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
-import BackgroundImage from '../public/Assets/3.jpg'
 import Navbar from './components/navbar'
 import Programs from './components/programs';
 function Index(){
     return(
         <div style={{position: 'relative'}}>
-        <img src={BackgroundImage} style={{width: '100%', height : '90vh',objectFit : 'cover'}} />
+        <img src="/Humanity-Welfare-Limited/Assets/3.jpg" style={{width: '100%', height : '90vh', objectFit : 'cover'}} />
+
         <Navbar />
         <Programs />
         </div>
